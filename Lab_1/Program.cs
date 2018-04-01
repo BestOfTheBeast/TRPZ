abcb6e2d51a0dbec83b439d9cec1aeaf68d45240
Maxim Kazarin <kazzarin99@gmail.com>
2018-04-01 08:02:56 +0300

    Verification deleted

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -10,13 +10,7 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
-            int[] arr = { 2, 20, 12, 44, 12, 56, 5, 22 };
-            int[] result = SortetContainer.ShakerSort(arr);
-            for (int i = 0; i < result.Length; i++)
-            {
-                Console.Write(result[i] + " ");
-            }
-            Console.ReadKey();
+          
         }
     }
 }
